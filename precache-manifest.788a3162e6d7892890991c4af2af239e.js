945bba6cdd98de6fe4b1b7a9c6ff3083456a438f
Andres Mena <ballerlikemike23@gmail.com>
2019-07-04 17:55:07 -0400

    Updates

---
+++ precache-manifest.788a3162e6d7892890991c4af2af239e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c4b3906159109bf727b259d7ce7310",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "e784bbec9d3f5776ae5d",
+    "url": "/Monsters-Rolodex/static/css/main.9fddb4aa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e784bbec9d3f5776ae5d",
+    "url": "/Monsters-Rolodex/static/js/main.867540fc.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
